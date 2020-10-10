1a13eba41f26c71d5eb6c812603185856440d1c0
christina <xristitina@gmail.com>
2020-10-10 23:08:17 +0300

    Add modal styles

--- src/app/cookie-banner/components/modal/modal.component.ts
+++ src/app/cookie-banner/components/modal/modal.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input } from "@angular/core";
-import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html'
+    selector: 'app-modal',
+    templateUrl: './modal.component.html',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent {
-  // TODO: Add proper type
-  @Input() content: any;
+    // TODO: Add proper type
+    @Input() content: any;
 
-  isCollapsed: boolean = true;
+    isCollapsed: boolean = true;
 
-  constructor(public activeModal: NgbActiveModal) {}
+    constructor(public activeModal: NgbActiveModal) {}
 }
