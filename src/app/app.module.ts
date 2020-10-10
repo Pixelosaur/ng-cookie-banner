3ac80f82f68727a0eba71de06973414926c4c921
christina <xristitina@gmail.com>
2020-10-10 19:25:59 +0300

    Add collapse functionality on customize button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppComponent } from './app.component';
 import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CookieBannerParser } from './cookie-banner/parsers/cookie-banner.parser';
-import { NgbAlertModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
-import { ModalComponent } from "./cookie-banner/components/modal/modal.component";
+import { NgbAlertModule, NgbCollapseModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from './cookie-banner/components/modal/modal.component';
 
 @NgModule({
     declarations: [AppComponent, CookieBannerComponent, ModalComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbAlertModule, NgbModalModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        NgbAlertModule,
+        NgbModalModule,
+        NgbCollapseModule,
+    ],
     providers: [CookieBannerParser],
     bootstrap: [AppComponent],
 })
