30c2163a6b4d03925fa941fc4f81aa0004686353
christina <xristitina@gmail.com>
2020-10-10 10:28:55 +0300

    Modify cookie banner service

--- src/app/cookie-banner/services/cookie-banner.service.ts
+++ src/app/cookie-banner/services/cookie-banner.service.ts
@@ -1,20 +1,33 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { environment } from "../../../environments/environment";
-import { map } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
+
+import { CookieBanner } from '../interfaces/cookie-banner.interface';
+import { CookieBannerEnvelopeApiResponse } from '../interfaces/api/cookie-banner-envelope.api.interface';
+import { CookieBannerApiResponse } from '../interfaces/api/cookie-banner.api.interface';
+
+import { CookieBannerParser } from '../parsers/cookie-banner.parser';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class CookieBannerService {
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private cookieBannerParser: CookieBannerParser) {}
 
-  getPrivacySettings(): Observable<any> {
-    return this.http.get<any>(`${environment.serverUrl}/common/getBanner`).pipe(
-      map((response: any) => {
-        return response;
-      }),
-    );
-  }
+    getPrivacySettings(): Observable<CookieBanner[]> {
+        return this.http.get<any>(`${environment.serverUrl}/common/getBanner`).pipe(
+            map(
+                (response: CookieBannerEnvelopeApiResponse<CookieBannerApiResponse[]>) =>
+                    response.accordian,
+            ),
+            map((cookieBanners: CookieBannerApiResponse[]) => {
+                return cookieBanners.map((banner: CookieBannerApiResponse) =>
+                    this.cookieBannerParser.parseCookieBanner(banner),
+                );
+            }),
+        );
+    }
 }
