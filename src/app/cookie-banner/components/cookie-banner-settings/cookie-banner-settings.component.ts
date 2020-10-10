c241db9810022293fcba31a748ee783ee151d6d4
christina <xristitina@gmail.com>
2020-10-10 23:08:44 +0300

    Add cookie-banner-settings styles

--- src/app/cookie-banner/components/cookie-banner-settings/cookie-banner-settings.component.ts
+++ src/app/cookie-banner/components/cookie-banner-settings/cookie-banner-settings.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-cookie-banner-settings',
     templateUrl: './cookie-banner-settings.component.html',
+    styleUrls: ['./cookie-banner-settings.component.scss'],
 })
 export class CookieBannerSettingsComponent {
     isSettingsContentCollapsed: boolean = true;
