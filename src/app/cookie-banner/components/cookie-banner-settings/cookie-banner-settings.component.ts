167aa695c356f252a6ac3035b3c708de72c67e79
christina <xristitina@gmail.com>
2020-10-11 11:34:33 +0300

    Bind single banner settings to cookie banner settings component and iterate through plugin list

--- src/app/cookie-banner/components/cookie-banner-settings/cookie-banner-settings.component.ts
+++ src/app/cookie-banner/components/cookie-banner-settings/cookie-banner-settings.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CookieBanner } from '../../interfaces/cookie-banner.interface';
+import { CookieBannerPluginList } from '../../interfaces/cookie-banner-plugin-list.interface';
 
 @Component({
     selector: 'app-cookie-banner-settings',
     templateUrl: './cookie-banner-settings.component.html',
     styleUrls: ['./cookie-banner-settings.component.scss'],
 })
-export class CookieBannerSettingsComponent {
+export class CookieBannerSettingsComponent implements OnInit {
+    @Input() banner: CookieBanner;
+
+    pluginList: CookieBannerPluginList[] = [];
     isSettingsContentCollapsed: boolean = true;
 
     constructor() {}
+
+    ngOnInit(): void {
+        this.pluginList = this.banner?.pluginList;
+    }
 }
